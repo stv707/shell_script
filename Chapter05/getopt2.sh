227c9204748e69f0f79b8c7530f8986e6561a600
root <root@master.linuxvmimages.local>
2021-07-27 14:52:37 +0800

    v2

---
+++ Chapter05/getopt2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+USAGE="usage: $0 -x -y <arg> -z"
+
+
+if [ $# -lt 1 ] 
+then 
+	echo $USAGE
+	exit 12
+fi 
+
+
+while getopts :xy:z opt_char
+
+do
+	case $opt_char in
+	x)
+		echo "running cal"
+		cal 
+		;;
+	y)
+		echo "running cal with option"
+		cal $OPTARG
+		;;
+	z)	
+		echo "running date command"
+		date
+		;; 
+	*)
+		echo "$OPTARG is not a valid option."
+		echo "$USAGE"
+		;;
+	esac
+done

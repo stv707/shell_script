5f611ae979a33a72375286b7e4778930daf74ef4
root <root@master.linuxvmimages.local>
2021-07-28 12:08:26 +0800

    v2

--- Chapter08/if_03.sh
+++ Chapter08/if_03.sh
@@ -2,6 +2,10 @@ echo "Enter the first string to compare"
 read name1
 echo "Enter the Second string to compare"
 read name2
+
+
+
+
 if [ $name1 == $name2 ]
 then
 echo "First string is equal to Second string"

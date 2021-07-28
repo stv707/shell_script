b963e05d44a6957eafa6ee81f06e037da372c978
root <root@master.linuxvmimages.local>
2021-07-28 11:48:57 +0800

    v2

--- Chapter08/test06.sh
+++ Chapter08/test06.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+
+declare -i num1 num2  
+
 echo "Please enter 1st Number"
 read num1
 echo "Please enter 2nd Number"
 read num2
 echo
+
+
 test $num1 -eq $num2 # Test for Equal
 echo $?
+
 test $num1 -ne $num2 # Test for Not Equal
 echo $?
+
 test $num1 -ge $num2 # Test for Greater Than Equal
 echo $?
+
 echo "Please enter 1st String"
 read Str1
 echo "Please enter 2nd String"
@@ -18,5 +26,6 @@ test $Str1 = $Str2 # Test for Two Strings Are Equal
 echo $?
 test -z $Str1 # Test for The Length Of The String Is > 0
 echo $?
-test $Str2 # Test for The String Is Not NULL
+#test $Str2 # Test for The String Is Not NULL
+[ $Str2 ]  # Test for The String Is Not NULL
 echo $?

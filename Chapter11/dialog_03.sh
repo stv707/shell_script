fa706344443cdf76afb7637a3c2ef94936a47988
root <root@master.linuxvmimages.local>
2021-07-29 15:09:02 +0800

    v2

--- Chapter11/dialog_03.sh
+++ Chapter11/dialog_03.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 # creating the file to store password
-result="output.txt 2>/dev/null"
+
+result="output.txt"
+
 # delete the password stored file, if program is exited pre-maturely.
+
+
 trap "rm -f output.txt" 2 15
-dialog --title "Password" \
---insecure \
---clear \
---passwordbox "Please enter password" 10 30 2> $result
+
+
+dialog --title "Password" --insecure --clear --passwordbox "Please enter password" 10 30  2> $result
+
+
 reply=$?
+
 case $reply in
-0) echo "You have entered Password : $(cat $result)";;
-1) echo "You have pressed Cancel";;
-255) cat $data && [ -s $data ] || echo "Escape key is pressed.";;
+	0) clear && echo "You have entered Password : $(cat $result)" ;;
+	1) clear && echo "You have pressed Cancel";;
+	255) clear &&  echo "Escape key is pressed.";;
 esac
--- Chapter11/dialog_04.sh
+++ Chapter11/dialog_04.sh
@@ -6,18 +6,24 @@ TEMP_DATA=output.txt
 vi_editor=vi
 # trap and delete temp files
 trap "rm $TEMP_DATA; rm $RESPONSE; exit" SIGHUP SIGINT SIGTERM
-function display_output(){
-dialog --backtitle "Learning Shell Scripting" --title "Output"
---clear --msgbox "$(<$TEMP_DATA)" 10 41
+
+
+function display_output()
+{
+	dialog --backtitle "Learning Shell Scripting" --title "Output" --clear --msgbox "$(<$TEMP_DATA)" 10 41
 }
-function display_date(){
-echo "Today is `date` @ $(hostname -f)." >$TEMP_DATA
+function display_date()
+{
+echo "Today is `date` " >$TEMP_DATA
 display_output 6 60 "Date and Time"
 }
+
 function display_calendar(){
 cal >$TEMP_DATA
 display_output 13 25 "Calendar"
 }
+
+
 # We are calling infinite loop here
 while true
 do
@@ -33,13 +39,15 @@ Editor "Start vi editor" \
 Exit "Terminate the Script" 2>"${RESPONSE}"
 menuitem=$(<"${RESPONSE}")
 # Start activity as per selected choice
+
 case $menuitem in
-Calendar) display_calendar;;
-Date/time) display_date;;
-Editor) $vi_editor;;
-Exit) echo "Thank you !"; break;;
+     Calendar) display_calendar;;
+     Date/time) display_date;;
+     Editor) $vi_editor;;
+     Exit) echo "Thank you !"; break;;
 esac
 done
+clear
 # Delete temporary files
 [ -f $TEMP_DATA ] && rm $TEMP_DATA
 [ -f $RESPONSE ] && rm $RESPONSE
